a73a0321b3197eed45693d6de0d5d9c478b10c9e
balibeni <63418080+balibeni@users.noreply.github.com>
2022-03-08 20:08:55 +0100

    Update delete.php

--- forras/client/delete.php
+++ forras/client/delete.php
@@ -4,35 +4,3 @@
 <input type="button" value="Törlés" id="torles" class="bg-danger">
 <div id="msg"></div>
 </div>
-<script>
-    function adatok(){
-      let id=document.getElementById("faz").value
-     
-      fetch("../server/kivfa.php?faz="+id)
-        .then((response) => response.json())
-        .then((data) => faadatok(data));
-    }
-function faadatok(data){
-    if(data.length ==0){
-        document.getElementById("msg").innerHTML="Nincs ilyen fa az adatbázisban";
-        let gomb=document.getElementById("torles")
-       
-        gomb.disabled="true";
-        
-    }else{
-     let gomb=document.getElementById("torles")
-    gomb.disabled=false;
-    document.getElementById("msg").innerHTML="A törlendő fa: "+data[0]['faj']+"<br> A fa települése: "+data[0]['telepules']+"<br> A fa körmérete: "+data[0]['kormeret'];
-}}
-document.getElementById("torles").addEventListener('click',delet)
-
-function delet(e){
-    let id=document.getElementById("faz").value
-    fetch("../server/delete.php?faz="+id)
-        .then((response) => response.text())
-        .then((data) => alert(data));
-    location.reload()
-}
-
-
-</script>
